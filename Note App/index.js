cb0d7b333d8120f0f5ede575b6e626a9acf443f9
Monye Jude <Judechuks26@gmail.com>
2023-04-18 08:43:46 +0100

    index.js

---
+++ Note App/index.js
@@ -0,0 +1,12 @@
+import App from "./App";
+import React from "react";
+// const react = require("react");
+import ReactDOM from "react-dom";
+// const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>, 
+  document.getElementById("root")
+);
